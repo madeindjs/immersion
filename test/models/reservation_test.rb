17b6dba7d7b418329012bc39c376df65f43533bd
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-02-16 09:40:35 +0100

    merge remote brnach

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should create status" do
+    r = Reservation.create user_id: 1, product_id: 1, start_on: '2016-01-01 12:00:00'
+    assert_same 1, r.statuses.size
+  end
 end
