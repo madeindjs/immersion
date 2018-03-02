4d717c01b5c74d8f6e6a171fc734b2b06bb976bd
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-03-02 13:11:14 +0100

    Create message to the correct guy

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :reservation
+
+
+  # Send message to the reservation author or product author
+  #
+  # @return User
+  def to
+    if self.reservation.user_id == self.user_id
+      # client send a message to the author
+      return self.reservation.product.user
+    end
+      return self.reservation.user
+    else
+  end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @me = users(:me)
+    @my_girlfriend = users(:my_girlfriend)
+    @reservation = reservations(:my_reservation_for_product_of_my_girlfriend)
+  end
+
+  test "send message to the product author" do
+    message = Message.new reservation_id: @reservation.id, user_id: @me.id
+
+    assert_equal @my_girlfriend.email, message.to.email
+  end
+
+  test "send message to the reservation author" do
+    message = Message.new reservation_id: @reservation.id, user_id: @my_girlfriend.id
+
+    assert_equal @me.email, message.to.email
+  end
+
 end
